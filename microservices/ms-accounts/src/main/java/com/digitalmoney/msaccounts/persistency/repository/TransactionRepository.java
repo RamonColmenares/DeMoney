637f28bb0d9106a005844c229c982862b85a1a75
Daniela Berardi <danielaberardi@live.com.ar>
2023-06-12 23:04:40 -0300

    24-db | add filter by date range and transaction type

--- microservices/ms-accounts/src/main/java/com/digitalmoney/msaccounts/persistency/repository/TransactionRepository.java
+++ microservices/ms-accounts/src/main/java/com/digitalmoney/msaccounts/persistency/repository/TransactionRepository.java
@@ -2,16 +2,23 @@ package com.digitalmoney.msaccounts.persistency.repository;
 
 import com.digitalmoney.msaccounts.persistency.entity.Transaction;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     @Query("SELECT new com.digitalmoney.msaccounts.application.dto.TransactionActivityDTO(t.amount, t.transactionDate, t.id, t.transactionType) from Transaction t where t.account.id = :idAccount order by t.transactionDate desc")
     Page<Transaction> getTransactionByAccountId(@RequestParam Long idAccount, Pageable pageable);
+
+    @Query("SELECT t from Transaction t where t.account.id = :idAccount AND t.transactionDate >= :startDate AND t.transactionDate <= :endDate order by t.transactionDate desc")
+    List<Transaction> getTransactionByAccountIdAndDateRange(@RequestParam Long idAccount, LocalDateTime startDate, LocalDateTime endDate);
+
+    @Query("SELECT t FROM Transaction t WHERE t.account.id = :idAccount AND t.transactionType = :transactionType ORDER BY t.transactionDate DESC")
+    List<Transaction> getTransactionsByAccountIdAndTransactionType(@RequestParam Long idAccount, Transaction.TransactionType transactionType);
 }
--- microservices/ms-accounts/src/main/java/com/digitalmoney/msaccounts/service/TransactionService.java
+++ microservices/ms-accounts/src/main/java/com/digitalmoney/msaccounts/service/TransactionService.java
@@ -1,22 +1,57 @@
 package com.digitalmoney.msaccounts.service;
 
+import com.digitalmoney.msaccounts.application.exception.InternalServerException;
 import com.digitalmoney.msaccounts.persistency.dto.TransactionResponseDTO;
 import com.digitalmoney.msaccounts.persistency.entity.Transaction;
 import com.digitalmoney.msaccounts.persistency.repository.TransactionRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service @AllArgsConstructor
 public class TransactionService {
     private final TransactionRepository repository;
 
+    public List<Transaction> findAll(){
+        return repository.findAll();
+    }
+
     public Page<TransactionResponseDTO> getTransactionByAccountId(Long idAccount, int page, int size) {
         return repository.getTransactionByAccountId(idAccount, PageRequest.of(page, size, Sort.by("transactionDate")))
                 .map(this::convertToDto);
+    }
+
+    public List<TransactionResponseDTO> getTransactionsByAccountIdAndDateRange(Long idAccount, LocalDate startDate, LocalDate endDate) throws InternalServerException {
+        LocalDateTime startDateTime = startDate.atTime(LocalTime.MIDNIGHT);
+        LocalDateTime endDateTime = endDate.atTime(LocalTime.MAX);
+
+        List<Transaction> transactions;
+        try {
+            transactions = repository.getTransactionByAccountIdAndDateRange(idAccount, startDateTime, endDateTime);
+        } catch (Exception e) {
+            throw new InternalServerException(e.getMessage());
+        }
+        return transactions.stream().map(this::convertToDto).collect(Collectors.toList());
+    }
+
+    public List<TransactionResponseDTO> getTransactionsByAccountIdAndTransactionType(Long idAccount, String transactionTypeString) throws InternalServerException {
+        Transaction.TransactionType transactionType = Transaction.TransactionType.valueOf(transactionTypeString);
+
+        List<Transaction> transactions;
+        try {
+            transactions = repository.getTransactionsByAccountIdAndTransactionType(idAccount, transactionType);
+        } catch (Exception e) {
+            throw new InternalServerException(e.getMessage());
+        }
+        return transactions.stream().map(this::convertToDto).collect(Collectors.toList());
     }
 
     private TransactionResponseDTO convertToDto(Transaction transaction) {
